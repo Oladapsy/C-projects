db430a258de2e913ac76d45af6ed969b4c051b56
oladapsy <dapoodedeyi@yahoo.com>
2023-05-04 15:25:29 +0000

    this is the first main

---
+++ main1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ *main: this main is just to print text to the screen
+ *
+ *description- the print is a already declared function in our stdio header
+ *		that helps it print out input to the standard output
+ *
+ *Return: return 0 on success
+ */
+
+int main()
+{
+	printf("This printf function prints to the screen");
+	return (0);
+}
