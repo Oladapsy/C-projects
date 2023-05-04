e6c1ef42cf5da1a1aad341a1191360d7eee4f95b
oladapsy <dapoodedeyi@yahoo.com>
2023-05-04 15:54:59 +0000

    this will help with using tabs escape

---
+++ usingtab.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main: this is just to use some escape with tab
+ *
+ * description: this allows you to use the tab or show you its usage
+ *
+ * Return:  (0) on success.
+ */
+int main()
+{
+	printf("1\t2\t3\t\n4\t5\t6\t\n7\t8\t9\t");
+	return (0);
+	/* note that the \t = tab and \n = new line, now lets see the output*/
+}
