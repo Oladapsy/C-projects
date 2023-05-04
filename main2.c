7bcc38132c23769c83d5f4048a9b75b86cf01434
oladapsy <dapoodedeyi@yahoo.com>
2023-05-04 15:45:26 +0000

    this is a file for commenting and escape '/'

---
+++ main2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ *main: this is for commenting
+ *
+ *`description- this gives more light to comnmenting
+ *
+ * Return: 0 (on success)
+ */
+int main()
+{
+	printf("thanks for the commenting\" and tab");
+	return (0);
+	//this is another orm of comment for a single line
+	//and this or another line
+	/* for a single or multiline comment, this can be used as well*/
+	/*this is a sample
+	 *when it is used as a multi comment
+	 */
+	/*the / backslash helps as an escape key for /"" in printf */
+}
